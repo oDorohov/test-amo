9d0956cc2f9db0fd8b1b5b40768feddc11d0bde5
oDorohov <o9158554533@yandex.ru>
2025-05-12 09:24:16 +0300

    Update params.php

--- config/params.php
+++ config/params.php
@@ -5,8 +5,8 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
 	'amo' => [
-        'clientId' => 'ba1af289-74a1-45bf-a4ff-dc0ea1ddef37',
-        'clientSecret' => '9nylLrCP4lxgX9stWrzcsNOICazB7MxJQbw1OwlIPXegm97nq8pXjbSLXn824WG5',
+        'clientId' => '**',
+        'clientSecret' => '**',
         'redirectUri' => 'https://test-yii.ru/amo/callback',
         'domain' => 'o9158554533.amocrm.ru',
 		'tokenPath' =>'@runtime/json_token.json',
